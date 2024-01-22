2e5ddc88a2b97b3c9bfb633ed87fba7f3523b6ef
Mohommed Rimaz <rimazmohommed523@gmail.com>
2024-01-22 22:22:04 +1300

    add console app and wire  up IOptions from new appsetting file

---
+++ src/ConsoleApp/Application.cs
@@ -0,0 +1,27 @@
+﻿using Application.Pets.Queries.GetPets;
+using ConsoleApp.Interfaces;
+using MediatR;
+
+namespace ConsoleApp
+{
+    public class Application : IApplication
+    {
+        private readonly IMediator _mediator;
+
+        public Application(IMediator mediator) 
+        { 
+            _mediator = mediator;
+        }
+
+        public async Task Run()
+        {
+            var result = await _mediator.Send(new GetAvailablePetsQuery());
+            foreach (var pet in result)
+            {
+                Console.WriteLine(pet.Id);
+                Console.WriteLine(pet.Name);
+                Console.WriteLine(pet.Status);
+            }
+        }
+    }
+}
---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using Application;
+using Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using ConsoleApp.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Infrastructure.Common;
+
+namespace ConsoleApp
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+            
+            //Wire up IOptions for Console App
+            builder.Configuration.Sources.Clear();
+            builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            builder.Configuration.GetSection(nameof(IntegrationOptions)).Bind(new IntegrationOptions());
+
+            //Add Application and Infrastrucure layer dependency injection
+            builder.Services.AddApplication();
+            builder.Services.AddInfrastructure();
+
+            //Add console application and run
+            builder.Services.AddScoped<IApplication, Application>();
+            IHost _host = builder.Build();
+            var app = _host.Services.GetRequiredService<IApplication>();
+            app.Run().Wait();
+        }
+    }
+}
