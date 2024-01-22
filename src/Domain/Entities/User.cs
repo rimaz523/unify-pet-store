9ca3bbb9c449b265a3f086c1b1aae71ab0c9cc57
Mohommed Rimaz <rimazmohommed523@gmail.com>
2024-01-22 16:58:56 +1300

    remove unused scaffolding

--- src/Domain/Entities/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Domain.Entities;
-
-public class User
-{
-    public Guid Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-}
