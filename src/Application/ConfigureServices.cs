bedb7f6c66a925e5a8fc7ac87b09340948b9ae1d
Mohommed Rimaz <rimazmohommed523@gmail.com>
2024-01-22 16:39:43 +1300

    initial commit creating a .net app from cla-sln template

---
+++ src/Application/ConfigureServices.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        });
+        return services;
+    }
+}
